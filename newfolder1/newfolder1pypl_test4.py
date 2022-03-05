fffd7328e916915330702b7a5e3b3e32c19115d5
Vignesh Nagarajan <vignesh4iphone@gmail.com>
2022-03-05 09:11:51 -0800

    Additional for loop statement added to file newfolder1pypl_test4.py

--- newfolder1/newfolder1pypl_test4.py
+++ newfolder1/newfolder1pypl_test4.py
@@ -1,1 +1,4 @@
-print(Hi, Im new file in folder newfolder1)
+print("Hi, Im new file in folder newfolder1")
+for i in range(5):
+    print(f"This is a test for loop counting the iterations\n this is the {i} iteration")
+
